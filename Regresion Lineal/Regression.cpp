17da5d5958a0c7fb45f6a2e8ed0e1a27913e1eaf
Avillamizarv <108444542+Avillamizarv@users.noreply.github.com>
2023-12-01 22:43:41 -0500

    Update Regression.cpp
    Add documentation

--- Regresion Lineal/Regression.cpp
+++ Regresion Lineal/Regression.cpp
@@ -2,27 +2,34 @@
 #include <fstream>
 #include <vector>
 
+// Estructura para almacenar los resultados del modelo  de la regresión lineal
 struct LinearRegressionResult {
-    double slope;    
-    double intercept;  
+    double slope;     // Pendiente
+    double intercept;  // Intersección
 };
 
+// Función para predecir el resultado usando los resultados de la regresión lineal
 double predict(const double& feature, const LinearRegressionResult& result) {
     return result.slope * feature + result.intercept;
 }
 
+// Función para entrenar el modelo de regresión lineal
 LinearRegressionResult trainModel(const std::vector<double>& X, const std::vector<double>& y, int epochs, double learningRate) {
     size_t numSamples = X.size();
 
     double slope = 0.0;
     double intercept = 0.0;
 
+    // Iterar a través de los epochs
     for (int epoch = 0; epoch < epochs; ++epoch) {
         for (size_t i = 0; i < numSamples; ++i) {
+            // Realizar una predicción
             double y_pred = predict(X[i], {slope, intercept});
-
+            
+            // Calcular el error
             double error = y_pred - y[i];
 
+            // Actualizar los parámetros (pendiente e intersección) utilizando el descenso de gradiente
             slope -= learningRate * error * X[i];
             intercept -= learningRate * error;
         }
@@ -32,16 +39,21 @@ LinearRegressionResult trainModel(const std::vector<double>& X, const std::vecto
 }
 
 int main() {
+    
+    // Abrir el archivo de entrada
     std::ifstream inputFile("input.txt");
     if (!inputFile.is_open()) {
         std::cerr << "Error al abrir el archivo de entrada." << std::endl;
         return 1;
     }
 
+    // Vectores para almacenar los datos de entrada (X) y salida (y)
     std::vector<double> X;
     std::vector<double> y;
     double value;
 
+    
+    // Leer los datos del archivo de entrada
     while (inputFile >> value) {
         X.push_back(value);
         inputFile >> value;
@@ -50,10 +62,14 @@ int main() {
 
     inputFile.close();
 
+    
+    // Configurar los parámetros del modelo
     int epochs = 1000;
     double learningRate = 0.01;
+    // Entrenar el modelo de regresión lineal
     LinearRegressionResult result = trainModel(X, y, epochs, learningRate);
 
+    // Escribir los resultados en el archivo de salida
     std::ofstream outputFile("output.txt");
     if (!outputFile.is_open()) {
         std::cerr << "Error al abrir el archivo de salida." << std::endl;
